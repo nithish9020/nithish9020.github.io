0028db63a5c9a64d899ec5f7d774b87819c6807d
Nithishkumar A <83629145+nithish9020@users.noreply.github.com>
2024-02-29 00:18:33 +0530

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,32 @@
+function generatePassword(){
+    const length = document.getElementById("nof").value;
+    const includeLowercase = true;
+    const includeUppercase = true;
+    const includeNumbers = true;
+    const includeSymbols = true;
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const symbolChars = "!@#$%^&*()_+-=";
+    const numberChars = "0123456789";
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase? lowercaseChars : "";
+    allowedChars += includeUppercase? uppercaseChars : "";
+    allowedChars += includeNumbers? numberChars : "";
+    allowedChars += includeSymbols? symbolChars : "";
+    if(length <= 0){
+        return 'Password length must be at least 1';
+    }
+    for(let i=0 ; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password+=allowedChars[randomIndex]
+    }
+
+    //console.log(allowedChars);
+    document.getElementById("pass").innerText=password;
+    return password;
+}
 (function($) {
   "use strict"; // Start of use strict
 
