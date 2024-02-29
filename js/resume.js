34ca19f664d583abf6d3d25850bf75d73dc6a9c7
Nithishkumar A <83629145+nithish9020@users.noreply.github.com>
2024-02-29 19:21:25 +0530

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -1,28 +1,35 @@
 function generatePassword(){
-  const length = document.getElementById("nof").value;
-  const includeLowercase = document.getElementById("lower").checked;
+    const length = document.getElementById("nof").value;
+    const includeLowercase = document.getElementById("lower").checked;
   const includeUppercase = document.getElementById("upper").checked;
   const includeNumbers = document.getElementById("numm").checked;
   const includeSymbols = document.getElementById("sym").checked;
-  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const symbolChars = "!@#$%^&*()_+-=";
-  const numberChars = "0123456789";
-  let allowedChars = "";
-  let password = "";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const symbolChars = "!@#$%^&*()_+-=";
+    const numberChars = "0123456789";
+    let allowedChars = "";
+    let password = "";
 
-  allowedChars += includeLowercase? lowercaseChars : "";
-  allowedChars += includeUppercase? uppercaseChars : "";
-  allowedChars += includeNumbers? numberChars : "";
-  allowedChars += includeSymbols? symbolChars : "";
-  if(length <= 0){
-      return 'Password length must be at least 1';
-  }
-  for(let i=0 ; i < length; i++){
-      const randomIndex = Math.floor(Math.random() * allowedChars.length);
-      password+=allowedChars[randomIndex]
-  }
+    allowedChars += includeLowercase? lowercaseChars : "";
+    allowedChars += includeUppercase? uppercaseChars : "";
+    allowedChars += includeNumbers? numberChars : "";
+    allowedChars += includeSymbols? symbolChars : "";
+    if(allowedchars = ""){
+      document.getElementById("pass").innerText = 'Please Select atleast only one type of character';
+    }
+    if(length <= 0){
+        document.getElementById("pass").innerText = 'Password length must be at least 1';
+    }
+    for(let i=0 ; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password+=allowedChars[randomIndex]
+    }
 
+    //console.log(allowedChars);
+    document.getElementById("pass").innerText=password;
+    return password;
+}
 (function($) {
   "use strict"; // Start of use strict
 
